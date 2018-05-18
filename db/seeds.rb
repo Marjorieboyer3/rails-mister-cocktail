d1c6a579c5debcffd64ec88b00197ae1bbfc37dc
marjorie boyer <marjorie.boyer3@gmail.com>
2018-05-18 11:23:29 +0200

    Prepare for Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Dry Vermouth")
+Ingredient.create(name: "Amaretto")
+Ingredient.create(name: "Tea")
+Ingredient.create(name: "strowberry")
+Ingredient.create(name: "Champagne")
+Ingredient.create(name: "Bourbon")
+Ingredient.create(name: "orange juice")
+Ingredient.create(name: "curaçao")
+Ingredient.create(name: "apple juice")
+
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Mateito")
